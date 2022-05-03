1e091d158c30dc18de8d3e56110d1ac41f04191f
nikrawlinson <70710354+nikrawlinson@users.noreply.github.com>
2022-05-03 18:13:13 +0100

    Initial upload

---
+++ timelapse.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+from gpiozero import MotionSensor
+from picamera import PiCamera
+from ftplib import FTP
+
+pir = MotionSensor(4)
+    
+def thegrab():
+    thetime = datetime.now()
+    detectiontime = thetime.strftime("%y-%m-%d-%H-%M-%S")
+    extension = ".jpg"
+    filename = detectiontime + extension
+    camera = PiCamera()
+    #camera.rotation = 270
+    time.sleep(2)
+    camera.capture(filename)
+    camera.close()
+    ftp = FTP('[server address]')
+    ftp.login('[username]','[password]')
+    ftp.cwd('')
+    ftp.storbinary("STOR " + filename, open(filename, 'rb'))
+    ftp.quit()
+    
+while True:
+    pir.wait_for_motion()
+    thegrab()
+    time.sleep(30)
